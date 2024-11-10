78a322180e6c6eec8b2cfb9ff1dc1c92a019c142
abhay pandit <109413847+abhaypanditzx@users.noreply.github.com>
2024-11-10 06:24:04 +0000

    minor change

--- src/pages/CareServices.jsx
+++ src/pages/CareServices.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-
+import doctorPaitent from '../assets/doctorPaitent.png'
 const CareServices = () => {
   return (
-    <div>CareServices</div>
+    <div className='bg-[#222222] min-h-[500px] max-h-[700px] flex justify-around '>
+      <div className=" max-w-[800px]  w-[800px]">
+     <h1 className='text-white/50 xl:text-6xl uppercase font-bold  '> Experienced Caregivers for Elderly Seniors</h1>
+      
+      </div>
+    <img src={doctorPaitent} className='max-w-[300px]' alt="" />
+    </div>
   )
 }
 
